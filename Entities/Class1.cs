d34954129a0c5d4005210c616a2d1b1e50412e65
itdev-lenovo <it1devops@gmail.com>
2020-03-27 17:04:06 +0530

    files added

---
+++ Entities/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+   public  class Class1
+    {
+        //Create tables Person and Company in the ASP .Net application. Define many to many relationship between the tables using Entity Framework.
+
+
+
+      
+
+    }
+}
